055c93b6b1390bb03392876ddc06e1c81608ec26
Junhwan Jang <junhwan16@gmail.com>
2021-03-15 13:12:28 +0900

    Update download_pretrained_models.sh

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -4,8 +4,7 @@ BASE_URL="http://download.tensorflow.org/models/object_detection/tf2/20200711/"
 MODEL = "ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
 
 
-wget --no-check-certificate \
-         ${BASE_URL}${MODEL}.tar.gz \
+wget ${BASE_URL}${MODEL}.tar.gz \
          -O ${MODEL}.tar.gz
 
 tar -xvf ${MODEL}.tar.gz
