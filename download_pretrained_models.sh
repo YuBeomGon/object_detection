42bb960cf77de35d0816a85a7c792751e36db83b
Junhwan Jang <junhwan16@gmail.com>
2021-03-15 10:48:25 +0900

    change URL name to be correct

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#/usr/bin/bash
 
 BASE_URL="http://download.tensorflow.org/models/object_detection/"
 MODEL="ssdlite_mobilenet_v2_coco_2018_05_09"
 
 wget --no-check-certificate \
          ${BASE_URL}${MODEL}.tar.gz \
-         -O /tmp/${MODEL}.tar.gz
+         -O ${MODEL}.tar.gz
 
-tar xzvf /tmp/${model}.tar.gz
+tar -xvf ${MODEL}.tar.gz
