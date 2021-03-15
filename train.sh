ba4bfd2f597abc1c9f711277d1fff98d723ea13a
Junhwan Jang <junhwan16@gmail.com>
2021-03-15 13:27:21 +0900

    Update train.sh
    -tf2.0

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 PIPELINE_CONFIG_PATH=configs/ssd_mobilenet_v2_fpnlite_papsmear.config
 
 PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
-    python3 ./models/research/object_detection/model_main.py \
+    python3 ./models/research/object_detection/model_main_tf2.py \
             --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
             --num_train_steps=20000 \
             --sample_1_of_n_eval_samples=1 \
