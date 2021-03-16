79b5ab897198b17307a8e04c9678d2e610a681b9
Junhwan Jang <junhwan16@gmail.com>
2021-03-16 16:51:36 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,8 +3,9 @@
 PIPELINE_CONFIG_PATH=configs/ssd_mobilenet_v2_fpnlite_papsmear.config
 
 PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
-    python3 ./models/research/object_detection/model_main_tf2.py \
+    TF_CPP_MIN_LOG_LEVEL=2 CUDA_VISIBLE_DEVICES=0,1 python3 ./models/research/object_detection/model_main_tf2.py \
             --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+	    --model_dir=./training/
             --num_train_steps=20000 \
             --sample_1_of_n_eval_samples=1 \
             --alsologtostderr
