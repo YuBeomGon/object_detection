45a14e14530efbf848e5408705acf375edef5184
Junhwan Jang <junhwan16@gmail.com>
2021-03-17 12:52:32 +0900

    Update exporting tflite compat graph

--- export_graph.sh
+++ export_graph.sh
@@ -2,18 +2,17 @@
 
 MODEL_DIR=ssd_mobilenet_v2_fpnlite_papsmear
 PIPELINE_CONFIG_PATH=configs/${MODEL_DIR}.config
-CHECKPOINT_PREFIX=training/ckpt-21
+CHECKPOINT_DIR=training/
 OUTPUT_DIR=model_exported
 
 # clear old exported model
 rm -rf ${OUTPUT_DIR}
 
 PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
-TF_CPP_MIN_LOG_LEVEL=2 CUDA_VISIBLE_DEVICES=0,1 \
-    python3 ./models/research/object_detection/export_tflite_ssd_graph.py \
+TF_CPP_MIN_LOG_LEVEL=2 CUDA_VISIBLE_DEVICES=2,3 \
+    python3 ./models/research/object_detection/export_tflite_graph_tf2.py \
             --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-            --trained_checkpoint_prefix=${CHECKPOINT_PREFIX} \
+            --trained_checkpoint_dir=${CHECKPOINT_DIR} \
             --output_directory=${OUTPUT_DIR} \
-            --max_detections=150 \
-            --max_classes_per_detection=3 \
-            --detections_per_class=300
+            --max_detections=150
+
