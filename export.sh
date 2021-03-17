b60551f0957a45fbfc4aede123e2c5751f6a2a9e
Junhwan Jang <junhwan16@gmail.com>
2021-03-17 09:45:44 +0900

    Modifed export shell

--- export.sh
+++ export.sh
@@ -2,7 +2,7 @@
 
 MODEL_DIR=ssd_mobilenet_v2_fpnlite_papsmear
 PIPELINE_CONFIG_PATH=configs/${MODEL_DIR}.config
-CHECKPOINT_PREFIX=${MODEL_DIR}/ckpt-21
+CHECKPOINT_DIR=training/
 OUTPUT_DIR=model_exported
 
 # clear old exported model
@@ -12,5 +12,5 @@ PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
 TF_CPP_MIN_LOG_LEVEL=2 CUDA_VISIBLE_DEVICES=0,1 \
     python3 ./models/research/object_detection/exporter_main_v2.py \
             --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-            --trained_checkpoint_prefix=${CHECKPOINT_PREFIX} \
+            --trained_checkpoint_dir=${CHECKPOINT_DIR} \
             --output_directory=${OUTPUT_DIR}
